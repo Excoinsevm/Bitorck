aa3cd635b16e6cbea7daf44e18510dbfd097c8cb
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-30 21:33:52 +0000

    Create tokens.js

---
+++ tokens.js
@@ -0,0 +1,51 @@
+const tokens = [
+    {
+        address: "0xdce5726e3bc8e1f574416978279bb0ae62ab3c15",
+        image: "https://assets.geckoterminal.com/qfziuwt7lwhpzrasswkk9u4vp1s0",
+        website: "https://popcatrock.xyz",
+        telegram: "https://t.me/PopCatRock",
+        twitter: "https://twitter.com/PopCatRock"
+    },
+    {
+        address: "0x0fc14d74b66efa44accbbe284ccdd356039e59dc",
+        image: "https://assets.geckoterminal.com/usavghj8he8xvxzxxcb7v0qrh2nc",
+        website: "https://popswap.click",
+        telegram: "https://t.me/PopCatRock",
+        twitter: "https://twitter.com/PopCatRock"
+    },
+    {
+        address: "0xa056871e6796315c558280bff7e7f5d2c5b1f6fb",
+        image: "https://raw.githubusercontent.com/Excoinsevm/S/main/charm.png",
+        website: "https://www.luckycharmbitrock.net",
+        telegram: "https://t.me/LuckyCharmBitrock",
+        twitter: "https://x.com/luckycharmbrock"
+    },
+    {
+        address: "0x978adb9fbf147c1316b3c04230c88b356f3f4513",
+        image: "https://assets.geckoterminal.com/phlp791qjpcj27jpbuh2v0zveka0",
+        website: "https://steamboatwillie-bitrock.xyz",
+        telegram: "https://t.me/sbw_brock",
+        twitter: "https://x.com/Steamboat_BROCK"
+    },
+    {
+        address: "0xd647a6e1f898c39ba594443c19de0872ea935310",
+        image: "https://raw.githubusercontent.com/PopCatRock/token-list/main/src/tokens/CoinLogos/0xD647a6E1F898c39bA594443c19De0872EA935310.png",
+        website: "https://www.bit-rock.io",
+        telegram: "https://t.me/Bitrockchain",
+        twitter: "https://twitter.com/BitRockChain"
+    },
+    {
+        address: "0xfaf9a02db868cb557aa9ef6c9eeaac34afd013e2",
+        image: "https://raw.githubusercontent.com/Excoinsevm/swap-ui/refs/heads/master/IMG_9421.png",
+        website: "https://jammasarcade.com",
+        telegram: "https://t.me/jammasarcadeonbitrock",
+        twitter: "https://x.com/jammasarcade"
+    },
+    {
+        address: "0x9a6ddc9c7d873bfc20e34acb1aae41735a9856d6",
+        image: "https://raw.githubusercontent.com/PopCatRock/token-list/main/src/tokens/CoinLogos/0x9a6DdC9c7d873Bfc20E34ACb1AaE41735A9856d6.png",
+        website: "https://sadrock.site/#",
+        telegram: "https://t.me/SADROCK_NFTs",
+        twitter: "https://twitter.com/SADROCK_nft"
+    }
+];
